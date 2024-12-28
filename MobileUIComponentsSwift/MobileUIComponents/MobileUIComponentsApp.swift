b6d4001b64cc2873915abb4dbc700ed84affa971
Laura Franco <laura@digio.es>
2024-12-28 15:46:23 +0100

    - Updated documentation - Removed third party projects

---
+++ MobileUIComponentsSwift/MobileUIComponents/MobileUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileUIComponentsApp.swift
+//  MobileUIComponents
+//
+//  Created by Laura on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MobileUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A demo of every UI component available in SwiftUI and Jetpack Compose
 ## SwiftUI
 
 - [Official documentation](https://developer.apple.com/design/human-interface-guidelines/components)
+- [Showcase](https://swiftui.gallery)
 
 ## Jetpack Compose
 
